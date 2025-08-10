50eb7e3204c3f4a7b4ebce1a60e41a4a5602adbe
Maria Clara Medina Gomez <macla@Marias-MacBook-Pro.local>
2025-08-09 23:15:11 -0500

    agregar vista y template para listado de productos

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import homePageView, aboutPageView
+from .views import homePageView, aboutPageView, productIndexView,productShowView
 
 
 urlpatterns = [
     path('', homePageView.as_view(), name='home'),  
     path('about/', aboutPageView.as_view(), name='about'),
+    path('products/', productIndexView.as_view(), name='index'),
+    path('products/<str:id>', productShowView.as_view(), name='show'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
 
 class homePageView(TemplateView):
     template_name= 'pages/home.html'
@@ -19,3 +20,35 @@ class aboutPageView(TemplateView):
         "author": "Developed by: Maria Clara Medina Gómez",
         })
         return context
+    
+
+class Product:
+    products = [
+    {"id":"1", "name":"TV", "description":"Best TV"},
+    {"id":"2", "name":"iPhone", "description":"Best iPhone"},
+    {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
+    {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+    ]
+
+class productIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+
+        return render(request, self.template_name, viewData)
+
+
+class productShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        viewData = {}
+        product = Product.products[int(id)-1]
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] = product["name"] + " - Product information"
+        viewData["product"] = product
+        return render(request, self.template_name, viewData)

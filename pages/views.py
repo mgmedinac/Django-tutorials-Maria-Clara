1086663ca628cad04d4e6e3a840d4520a1523d93
Maria Clara Medina Gomez <macla@Marias-MacBook-Pro.local>
2025-08-09 22:37:47 -0500

    Termina el about con mi nombre

--- pages/views.py
+++ pages/views.py
@@ -8,3 +8,14 @@ class homePageView(TemplateView):
 
 class aboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+        "title": "About us - Online Store",
+        "subtitle": "About us",
+        "description": "This is an about page ...",
+        "author": "Developed by: Maria Clara Medina Gómez",
+        })
+        return context

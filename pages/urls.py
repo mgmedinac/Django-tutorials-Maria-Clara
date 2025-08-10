c54b31393b1f7f315f2c9661e4de6f7b135a5b10
Maria Clara Medina Gomez <macla@Marias-MacBook-Pro.local>
2025-08-09 22:32:01 -0500

    Agrega páginas Home y About con sus respectivas plantillas y rutas

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, aboutPageView
+
 
 urlpatterns = [
     path('', homePageView.as_view(), name='home'),  
+    path('about/', aboutPageView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 class homePageView(TemplateView):
     template_name= 'pages/home.html'
     
+
+
+class aboutPageView(TemplateView):
+    template_name = 'pages/about.html'
